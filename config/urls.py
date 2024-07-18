692dbbecc2524cacd446936ffdca6104bc24802a
imoken777 <s1f102301640@iniad.org>
2024-07-18 16:57:39 +0900

    終了機能のミス修正

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from todo import views as todo_views
 
 from todo import views as todo_views
 
@@ -25,7 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", todo_views.index, name="index"),
     path("<int:task_id>/", todo_views.detail, name="detail"),
-    path('<int:task_id>/update', todo_views.update, name='update'),
-    path('<int:task_id>/delete', todo_views.delete, name='delete'),
-    path('<int:task_id>/', todo_views.detail, name='close'),
+    path("<int:task_id>/update", todo_views.update, name="update"),
+    path("<int:task_id>/delete", todo_views.delete, name="delete"),
+    path("<int:task_id>/close", todo_views.close, name="close"),
 ]
